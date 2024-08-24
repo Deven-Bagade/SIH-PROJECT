0110b915b542475618c1bade3869044a335e384b
Deven-Bagade <devenbofficial@gmail.com>
2024-08-24 15:49:30 +0530

    homepage

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import './Container.css'; // Import the CSS file
+import './Container.css'; // Ensure this path is correct
+import logo from '../components/Group_6.png'; // Adjust based on actual extension
 
 function Container() {
   return (
-    <>
-    <h1>Recycle your E-WASTE Hassle free with us</h1>
+<div className='parentcontainer'>
+      <h1>Recycle your E-WASTE Hassle-free with us</h1>
 
-    <div className="container">
-      {/* Image div */}
-      <div className="imagediv">
-        <img src="/Group 6.jpg" alt="image" />
+      <div className="container">
+        <div className="imagediv">
+          <img src={logo} alt="Recycle E-WASTE" />
+        </div>
+
+        <div className="buttoncontainer">
+          <button className="rect-button">Button 1</button>
+          <button className="rect-button">Button 2</button>
+          <button className="rect-button">Button 3</button>
+          <button className="rect-button">Button 4</button>
+        </div>
+      </div>
+      <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rerum blanditiis ipsa nesciunt eligendi praesentium dolore, inventore soluta optio nihil. Delectus, quo? Cupiditate eos eius commodi. Sit dolorum deserunt delectus repudiandae tenetur. Vitae rem non ducimus quia et repudiandae corporis sint hic praesentium fuga. Numquam illo eaque nobis quibusdam atque!
+      </p>
+      </div>
       </div>
 
-      {/* Button container div */}
-      <div className="buttoncontainer">
-        <button className="rect-button">Button 1</button>
-        <button className="rect-button">Button 2</button>
-        <button className="rect-button">Button 3</button>
-        <button className="rect-button">Button 4</button>
-      </div>
-    </div>
-    </>
+     
   );
 }
 
